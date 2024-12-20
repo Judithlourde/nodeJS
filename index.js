12096b53c20f78d523237da00f147949ce7af244
jpe <jpe@bim2share.com>
2024-12-20 10:27:58 -0500

    Add functionality for qr generator

--- index.js
+++ index.js
@@ -17,7 +17,37 @@
 // console.log(`My name is: ${sillyName}.`);
 
 // import superheroes from 'superheroes';
-import { randomSuperhero } from 'superheroes';
+// import { randomSuperhero } from 'superheroes';
+import inquirer from 'inquirer';
+import fs from 'fs';
+import qr from 'qr-image';
 
-const superHeroName = randomSuperhero();
-console.log(`My superhero name is : ${superHeroName}.`);
+// const superHeroName = randomSuperhero();
+// console.log(`My superhero name is : ${superHeroName}.`);
+// let name;
+ 
+inquirer
+    .prompt([
+        /* Pass your questions in here */
+        { 
+            message: 'Hi! What is your URL?',
+            name: 'name'
+        }
+    ])
+    .then((answers) => {
+        const name = answers.name;
+        const qr_png = qr.image(name, { type: 'png' } );
+        qr_png.pipe(fs.createWriteStream(`${name}.png`));
+
+        fs.writeFile("my_qr.txt", name, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+    });
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log(error);
+        } else {
+            console.log('Something else went wrong');
+        }
+    });
